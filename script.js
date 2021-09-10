b7465dc080b87d1ae3edb133881bea1b7ebe4d78
anilkumar34-learntocode <80689436+anilkumar34-learntocode@users.noreply.github.com>
2021-09-10 15:49:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,107 +1,86 @@
 'use strict';
-
-const rOck = 'rock';
-const paper = 'paper';
-const sIcor = 'sicssor';
-const game = [rOck, paper, sIcor];
+let iteM;
+const game = ['rock', 'paper', 'sicssor'];
 const playGame = document.querySelector('.btn');
 const gamereset = document.querySelector('#games');
 let play = true;
+let seL;
+document.querySelector('.rock').addEventListener('click', function () {
+  seL = true;
+  iteM = game[0];
+  document.getElementById('first').style.background =
+    'no-repeat url(rock1.jpg) center';
+  document.getElementById('first').style.backgroundSize = '10rem 10rem';
+});
+document.querySelector('.paper').addEventListener('click', function () {
+  iteM = game[1];
+  document.getElementById('first').style.background =
+    'no-repeat url(paper.jpg) center';
+  document.getElementById('first').style.backgroundSize = '10rem 10rem';
+});
+document.querySelector('.sicssor').addEventListener('click', function () {
+  iteM = game[2];
+  document.getElementById('first').style.background =
+    'no-repeat url(sicssor.jpg) center';
+  document.getElementById('first').style.backgroundSize = '10rem 10rem';
+});
 
 playGame.addEventListener('click', function () {
   const randomElement1 = game[Math.floor(Math.random() * game.length)];
-  const randomElement2 = game[Math.floor(Math.random() * game.length)];
-  console.log(randomElement1, randomElement2);
+
   if (play) {
-    if (randomElement1 === game[0] && randomElement2 === game[1]) {
-      document.getElementById('first').style.background =
-        'no-repeat url(rock1.jpg) center';
-      document.getElementById('first').style.backgroundSize = '10rem 10rem';
-      document.getElementById('second').style.background =
-        'no-repeat url(paper.jpg) center';
-      document.getElementById('second').style.backgroundSize = '10rem 10rem';
-      document.querySelector(
-        '.heading'
-      ).textContent = `PLAYER 2 WINS . CLICK RESET`;
-      play = false;
-    } else if (randomElement1 === game[0] && randomElement2 === game[2]) {
-      document.getElementById('first').style.background =
-        'no-repeat url(rock1.jpg) center';
-      document.getElementById('first').style.backgroundSize = '10rem 10rem';
-      document.getElementById('second').style.background =
-        'no-repeat url(sicssor.jpg) center';
-      document.getElementById('second').style.backgroundSize = '10rem 10rem';
-      document.querySelector(
-        '.heading'
-      ).textContent = `PLAYER 1 WINS . CLICK RESET`;
-      play = false;
-    } else if (randomElement1 === game[1] && randomElement2 === game[0]) {
-      document.getElementById('first').style.background =
-        'no-repeat url(paper.jpg) center';
-      document.getElementById('first').style.backgroundSize = '10rem 10rem';
+    if (randomElement1 === game[0] && iteM === game[1]) {
       document.getElementById('second').style.background =
         'no-repeat url(rock1.jpg) center';
       document.getElementById('second').style.backgroundSize = '10rem 10rem';
       document.querySelector(
         '.heading'
-      ).textContent = `PLAYER 1 WINS . CLICK RESET`;
+      ).textContent = `PLAYER WINS . CLICK RESET`;
       play = false;
-    } else if (randomElement1 === game[1] && randomElement2 === game[2]) {
-      document.getElementById('first').style.background =
-        'no-repeat url(paper.jpg) center';
-      document.getElementById('first').style.backgroundSize = '10rem 10rem';
-      document.getElementById('second').style.background =
-        'no-repeat url(sicssor.jpg) center';
-      document.getElementById('second').style.backgroundSize = '10rem 10rem';
-      document.querySelector(
-        '.heading'
-      ).textContent = `PLAYER 2 WINS . CLICK RESET`;
-      play = false;
-    } else if (randomElement1 === game[2] && randomElement2 === game[0]) {
-      document.getElementById('first').style.background =
-        'no-repeat url(sicssor.jpg) center';
-      document.getElementById('first').style.backgroundSize = '10rem 10rem';
+    } else if (randomElement1 === game[0] && iteM === game[2]) {
       document.getElementById('second').style.background =
         'no-repeat url(rock1.jpg) center';
       document.getElementById('second').style.backgroundSize = '10rem 10rem';
-      document.querySelector(
-        '.heading'
-      ).textContent = `PLAYER 2 WINS . CLICK RESET`;
+      document.querySelector('.heading').textContent = `CPU WINS . CLICK RESET`;
       play = false;
-    } else if (randomElement1 === game[2] && randomElement2 === game[1]) {
-      document.getElementById('first').style.background =
-        'no-repeat url(sicssor.jpg) center';
-      document.getElementById('first').style.backgroundSize = '10rem 10rem';
+    } else if (randomElement1 === game[1] && iteM === game[0]) {
+      document.getElementById('second').style.background =
+        'no-repeat url(paper.jpg) center';
+      document.getElementById('second').style.backgroundSize = '10rem 10rem';
+      document.querySelector('.heading').textContent = `CPU WINS . CLICK RESET`;
+      play = false;
+    } else if (randomElement1 === game[1] && iteM === game[2]) {
       document.getElementById('second').style.background =
         'no-repeat url(paper.jpg) center';
       document.getElementById('second').style.backgroundSize = '10rem 10rem';
       document.querySelector(
         '.heading'
-      ).textContent = `PLAYER 1 WINS . CLICK RESET`;
+      ).textContent = `PLAYER WINS . CLICK RESET`;
       play = false;
-    } else {
+    } else if (randomElement1 === game[2] && iteM === game[0]) {
+      document.getElementById('second').style.background =
+        'no-repeat url(sicssor.jpg) center';
+      document.getElementById('second').style.backgroundSize = '10rem 10rem';
+      document.querySelector(
+        '.heading'
+      ).textContent = `PLAYER WINS . CLICK RESET`;
+      play = false;
+    } else if (randomElement1 === game[2] && iteM === game[1]) {
+      document.getElementById('second').style.background =
+        'no-repeat url(sicssor.jpg) center';
+      document.getElementById('second').style.backgroundSize = '10rem 10rem';
+      document.querySelector('.heading').textContent = `CPU WINS . CLICK RESET`;
+      play = false;
+    } else if (randomElement1 === iteM) {
       if (randomElement1 === game[0]) {
-        document.getElementById('first').style.background =
+        document.getElementById('second').style.background =
           'no-repeat url(rock1.jpg) center';
-        document.getElementById('first').style.backgroundSize = '10rem 10rem';
+        document.getElementById('second').style.backgroundSize = '10rem 10rem';
       } else if (randomElement1 === game[1]) {
-        document.getElementById('first').style.background =
+        document.getElementById('second').style.background =
           'no-repeat url(paper.jpg) center';
-        document.getElementById('first').style.backgroundSize = '10rem 10rem';
+        document.getElementById('second').style.backgroundSize = '10rem 10rem';
       } else if (randomElement1 === game[2]) {
-        document.getElementById('first').style.background =
-          'no-repeat url(sicssor.jpg) center';
-        document.getElementById('first').style.backgroundSize = '10rem 10rem';
-      }
-      if (randomElement2 === game[0]) {
-        document.getElementById('second').style.background =
-          'no-repeat url(rock1.jpg) center';
-        document.getElementById('second').style.backgroundSize = '10rem 10rem';
-      } else if (randomElement2 === game[1]) {
-        document.getElementById('second').style.background =
-          'no-repeat url(paper.jpg) center';
-        document.getElementById('second').style.backgroundSize = '10rem 10rem';
-      } else if (randomElement2 === game[2]) {
         document.getElementById('second').style.background =
           'no-repeat url(sicssor.jpg) center';
         document.getElementById('second').style.backgroundSize = '10rem 10rem';
@@ -111,6 +90,9 @@ playGame.addEventListener('click', function () {
       ).textContent = `GAME IS DRAW CLICK RESET`;
       play = false;
     }
+  } else {
+    document.querySelector('.heading').textContent = `PLEASE CLICK RESET`;
+    play = false;
   }
 });
 gamereset.addEventListener('click', function () {
@@ -118,4 +100,9 @@ gamereset.addEventListener('click', function () {
   document.querySelector('#first').style.backgroundImage = 'url(psr.jpg)';
   document.querySelector('#second').style.backgroundImage = 'url(psr.jpg)';
   document.querySelector('.heading').textContent = `ROCK,PAPER,SISCCOR`;
+  if ((iteM = '')) {
+    play = false;
+  } else {
+    play = true;
+  }
 });
